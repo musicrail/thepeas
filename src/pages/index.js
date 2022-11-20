51dcf935f6f995919f72b9eaa5aae702d3bc7d21
Torben <torben.pfohl@gmail.com>
2022-11-20 13:29:27 +0100

    Added an image and plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>this is the main landing page.</p>
+      <StaticImage
+        src='../images/dog.jpeg' 
+        alt='Dog'
+      />
     </Layout>
   )
 }
