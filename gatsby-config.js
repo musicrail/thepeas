4af93bbdfd71a46d573585dc874829802eb621f6
Torben <torben.pfohl@gmail.com>
2022-11-23 01:49:10 +0100

    gatsby-config string to literal 2.

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,15 +8,15 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    'gatsby-plugin-image',
-    'gatsby-plugin-sharp',
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
         path: `${__dirname}/blog`,
-      }
+      },
     },
-    'gatsby-plugin-mdx',
+    "gatsby-plugin-mdx",
   ],
-}
+};
