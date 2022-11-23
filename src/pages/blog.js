97ea0ace7bf9efa9656c949a6ff77768d04bea9a
Torben <torben.pfohl@gmail.com>
2022-11-23 01:12:35 +0100

    nice, on a roll; added posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,25 +2,40 @@ import * as React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import { postLastModified } from "./blog.module.css";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="Blog posts">
-      {/* <Seo pageTitle={"he"} /> */}
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+        {
+          data.allMdx.nodes.map((node) => (
+            <article key={node.id}>
+              <h2>{node.frontmatter.name}</h2>
+              <p>Posted: {node.frontmatter.datePublished}</p>
+              <p>{node.excerpt}</p>
+              <p className={postLastModified}>Last modified on: {node.parent.modifiedTime}</p>
+            </article>
+          ))
+        }
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx (sort: { frontmatter: { datePublished: DESC } }) {
       nodes {
-        name
+        id
+        excerpt
+        frontmatter {
+          name
+          datePublished(formatString: "MMMM D, YYYY")
+        }
+        parent {
+          ... on File {
+            modifiedTime(formatString: "MMMM D, YYYY")
+          }
+        }
       }
     }
   }
